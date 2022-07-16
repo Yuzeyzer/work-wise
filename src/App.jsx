def8926c7b3ff3bc7d16dba9d60353cb48bf723b
Atabek <kurbanof.atabek@gmail.com>
2022-07-16 18:56:26 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Header } from "./components/Header/Header";
+import { Home } from "./pages/Home/Home";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="wrapper">
+        <Header />
+        <Home />
+      </div>
+    </div>
+  );
+}
+
+export default App;
